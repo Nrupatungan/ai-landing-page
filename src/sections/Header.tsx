7b097f7ef351711b559b2c706a5f043aa8c32094
Dinesh Sake <dineshsake12345@gmail.com>
2025-03-09 15:27:10 +0530

    Added proper backdrop blur effect to header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,9 +4,9 @@ import Button from '@/components/Button';
 
 export const Header = () => {
   return (
-    <header className='py-4 border-b border-white/15 md:border-none sticky top-0 z-10'>
+    <header className='py-4 border-b border-white/15 md:border-none sticky top-0 z-10 max-md:backdrop-blur'>
       <div className="container">
-        <div className='flex justify-between items-center md:border md:border-white/15 md:p-2.5 md:rounded-xl max-w-2xl mx-auto backdrop-blur'>
+        <div className='flex justify-between items-center md:border md:border-white/15 md:p-2.5 md:rounded-xl max-w-2xl mx-auto md:backdrop-blur'>
           <div className=''>
             <div className='border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15'>
               <LogoIcon className="h-8 w-8" />

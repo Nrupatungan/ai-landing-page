d2a14d23d764714495143a9b0e37675e4b737b55
Dinesh Sake <dineshsake12345@gmail.com>
2025-03-14 13:05:52 +0530

    added animation to testimonials and decoupled the testimonails for better readibility

---
+++ src/data/testimonials.ts
@@ -0,0 +1,39 @@
+import avatar1 from "@/assets/avatar-1.png";
+import avatar2 from "@/assets/avatar-2.png";
+import avatar3 from "@/assets/avatar-3.png";
+import avatar4 from "@/assets/avatar-4.png";
+import { StaticImageData } from "next/image";
+
+export type  testimonialsProps  = Array<{
+    text: string,
+    name: string,
+    title: string,
+    avatarImg: StaticImageData,
+}>
+
+export const testimonials = [
+  {
+    text: "“These AI tools have completely revolutionized our SEO entire strategy overnight”",
+    name: "Jamie Lee",
+    title: "Founder @ Pulse",
+    avatarImg: avatar2,
+  },
+  {
+    text: "“This product has completely transformed how I manage my projects and deadlines”",
+    name: "Sophia Perez",
+    title: "Director @ Quantum",
+    avatarImg: avatar1,
+  },
+  {
+    text: "“The user interface is so intuitive and easy to use, it has saved us countless hours”",
+    name: "Alisa Hester",
+    title: "Product @ Innovate",
+    avatarImg: avatar3,
+  },
+  {
+    text: "“Our team's productivity has increased significantly since we started using this tool”",
+    name: "Alec Whitten",
+    title: "CTO @ Tech Solutions",
+    avatarImg: avatar4,
+  },
+];
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,35 +1,8 @@
-import avatar1 from "@/assets/avatar-1.png";
-import avatar2 from "@/assets/avatar-2.png";
-import avatar3 from "@/assets/avatar-3.png";
-import avatar4 from "@/assets/avatar-4.png";
-import Image from "next/image";
+"use client"
 
-const testimonials = [
-  {
-    text: "“These AI tools have completely revolutionized our SEO entire strategy overnight”",
-    name: "Jamie Lee",
-    title: "Founder @ Pulse",
-    avatarImg: avatar2,
-  },
-  {
-    text: "“This product has completely transformed how I manage my projects and deadlines”",
-    name: "Sophia Perez",
-    title: "Director @ Quantum",
-    avatarImg: avatar1,
-  },
-  {
-    text: "“The user interface is so intuitive and easy to use, it has saved us countless hours”",
-    name: "Alisa Hester",
-    title: "Product @ Innovate",
-    avatarImg: avatar3,
-  },
-  {
-    text: "“Our team's productivity has increased significantly since we started using this tool”",
-    name: "Alec Whitten",
-    title: "CTO @ Tech Solutions",
-    avatarImg: avatar4,
-  },
-];
+import { testimonials } from "@/data/testimonials";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 export const Testimonials = () => {
   return (
@@ -38,9 +11,22 @@ export const Testimonials = () => {
         <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Beyond Expectations.</h2>
         <p className="mt-5 text-center text-lg md:text-xl tracking-tight text-white/70 max-w-xs mx-auto">Our revolutionary AI SEO tools have transformed our client&apos;s strategies.</p>
 
-        <div className="overflow-hidden mt-10 pl-5 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-              {testimonials.map(testimonial => (
+        <div className="flex overflow-hidden mt-10 pl-5 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div className="flex gap-5 pr-5 flex-none"
+          initial={{
+            translateX: "-50%",
+          }}
+          animate={{
+            translateX: "0%"
+          }}
+          transition={{
+            duration: 40,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "loop"
+          }}
+          >
+              {[...testimonials, ...testimonials].map((testimonial)  => (
                 <div key={testimonial.name} className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
                   <p className="text-lg md:text-2xl tracking-tight">{testimonial.text}</p>
                   <div className="flex items-center gap-3 mt-5">
@@ -54,7 +40,7 @@ export const Testimonials = () => {
                   </div>
                 </div>
               ))}
-            </div>
+            </motion.div>
         </div>
       </div>
     </section>
